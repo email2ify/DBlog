5accde65f73151c009ae84f965a34885aa77dc93
email2ify <email2ify@yahoo.com>
2023-03-15 18:50:31 +0000

    debug

--- dblog/settings.py
+++ dblog/settings.py
@@ -77,10 +77,7 @@ WSGI_APPLICATION = 'dblog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "postgres://dncfdpku:DjeZPJ8e2TGYWLpQQd9LS3cSopy8QDjp@mouse.db.elephantsql.com/dncfdpku"))
 }
 
 
